7e9b7668e8ecff70d750df0b890feff86d2716de
gengocodes <paulcorsino28@gmail.com>
2026-03-10 11:59:48 +0800

    change debug level to false

--- django_coursera/settings.py
+++ django_coursera/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ee*n06n3bid0xr_4s%wbgzy3q29+7=-#g&=#%klvt!q7h!vik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
