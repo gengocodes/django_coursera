9fc2c4f8fbddc4cd912fb2d81a78f9e5c30262d6
gengocodes <paulcorsino28@gmail.com>
2026-03-14 12:36:25 +0800

    add working with templates

--- app/views.py
+++ app/views.py
@@ -16,16 +16,13 @@ def drinks(request, drink_name):
     return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
 
 def home(request):
-    return HttpResponse("Welcome to Little Lemon !")
+    return render(request, 'index.html')
 
 def about(request):
-    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
-    return render(request, "about.html", {'content': about_content})
+    return render(request, 'about.html')
 
 def menu(request):
-    menu_items = Menu.objects.all()
-    items_dict = {"menu": menu_items}
-    return render(request, "menu.html", items_dict)
+    return render(request, 'menu.html')
 
 def book(request):
-    return HttpResponse("Make a booking")
+    return render(request, 'book.html')
